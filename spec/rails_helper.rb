abad822888684646e8de76e493433691d07e010c
nnks <ntomoya1010@gmail.com>
2017-09-25 22:22:55 +0900

    ref: add rspec module

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -50,4 +52,6 @@ RSpec.configure do |config|
 
   # ファクトリを簡単に呼び出す
   config.include FactoryGirl::Syntax::Methods
+  # ヘルパーマクロ
+  config.include LoginMacros
 end
